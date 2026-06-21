72024b313c6acd4c67885fe79f338b5f877c0cb9
Mauricio Sandim <mauriciosandim06@gmail.com>
2026-06-21 16:50:07 -0300

    Revert "feat: adicionar estrutura base e sistema de cadastro e login"

--- backend/src/main/java/ufla/projeto_es/gestao_turmas/model/usuario/response/LoginResponseDTO.java
+++
@@ -1,4 +0,0 @@
-package ufla.projeto_es.gestao_turmas.model.usuario.response;
-
-public record LoginResponseDTO(String token) {
-}
--- backend/src/main/java/ufla/projeto_es/gestao_turmas/model/usuario/response/UsuarioResponseDTO.java
+++
@@ -1,6 +0,0 @@
-package ufla.projeto_es.gestao_turmas.model.usuario.response;
-
-import ufla.projeto_es.gestao_turmas.model.type.RoleEnum;
-
-public record UsuarioResponseDTO(String nome, String email, String roleName) {
-}
