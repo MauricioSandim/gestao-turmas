0d120e4ffa7d340d7fb5aa793f46560263cfb241
MauricioSandim <mauriciosandim06@gmail.com>
2026-06-22 23:40:48 -0300

    feat: add Dockerfile e variáveis para execução do docker file

--- backend/src/main/java/ufla/projeto_es/gestao_turmas/GestaoTurmasApplication.java
+++ backend/src/main/java/ufla/projeto_es/gestao_turmas/GestaoTurmasApplication.java
@@ -11,10 +11,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class GestaoTurmasApplication {
 
 	public static void main(String[] args) {
-		System.out.println("HOST=" + System.getenv("SPRING_DATASOURCE_HOST"));
-		System.out.println("PORT=" + System.getenv("SPRING_DATASOURCE_PORT"));
-		System.out.println("DB=" + System.getenv("SPRING_DATASOURCE_DATABASE"));
-
 		SpringApplication.run(GestaoTurmasApplication.class, args);
 	}
 
